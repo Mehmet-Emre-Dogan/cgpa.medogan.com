cca48e82f3ce5bb173ce434a0799312e71a5f0df
Mehmet Emre Doğan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2022-07-12 02:37:35 +0300

    ui improvements
    scroll to bottom btn added
    navbar shrinked

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,5 +1,6 @@
 //Get the button
 var mybutton = document.getElementById("btn-back-to-top");
+var bottomButton = document.getElementById("btn-back-to-bottom");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -10,13 +11,26 @@ function scrollFunction() {
     } else {
     mybutton.style.display = "none";
     }
+
+    var distanceFromBottom = document.body.scrollHeight - window.innerHeight - window.scrollY
+    console.log(distanceFromBottom)
+    if ( distanceFromBottom > 20) {
+        bottomButton.style.display = "block";
+    } else {
+        bottomButton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", topFunction);
+bottomButton.addEventListener("click", bottomFunction);
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+function bottomFunction() {
+    document.body.scrollTop = document.body.scrollHeight;
 }
